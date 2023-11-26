55fa2b97141a85ac16da8fdacc12820ba724db91
Arvedson <tomasarvedson@gmail.com>
2023-11-26 11:20:58 -0600

    browser HTTP requests with fetch

---
+++ web-server/public/js/app.js
@@ -0,0 +1,14 @@
+console.log("Client side js file is loaded")
+
+fetch("http://localhost:3000/weather?address=").then((response) =>{
+    response.json().then((data) =>{
+        if(data.error) {
+            console.log(data.error)
+
+        } else {
+            console.log(data.ubicacion)
+        }
+    
+    })
+})
+
